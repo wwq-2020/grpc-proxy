b82813257a6ca4f428c3a15c40dc5eba7eb22671
wangweiqing.1988 <wangweiqing.1988@bytedance.com>
2025-03-19 11:06:53 +0800

    feat: add hook for handler

--- handler.go
+++ handler.go
@@ -15,11 +15,76 @@ type Director func(ctx context.Context, method string) (context.Context, *grpc.C
 
 type handler struct {
 	director Director
+	hook     Hook
 }
 
-func NewHandler(director Director) grpc.StreamHandler {
+type noopHook struct {
+}
+
+func (noopHook) OnStart(ctx context.Context, method string) context.Context {
+	return ctx
+}
+
+func (noopHook) OnFinish(ctx context.Context, method string, err error) {
+
+}
+
+func (noopHook) OnClientMsg(ctx context.Context, method string) {
+
+}
+
+func (noopHook) OnBackendMsg(ctx context.Context, method string) {
+
+}
+
+func (noopHook) OnProxyToBackendFinish(ctx context.Context, method string, err error) {
+
+}
+
+func (noopHook) OnProxyToClientFinish(ctx context.Context, method string, err error) {
+
+}
+
+func (noopHook) OnDirect(ctx context.Context, method string) {
+
+}
+
+func (noopHook) OnNewClientStream(ctx context.Context, method string) {
+
+}
+
+var (
+	_ Hook = &noopHook{}
+)
+
+type Hook interface {
+	OnStart(ctx context.Context, method string) context.Context
+	OnFinish(ctx context.Context, method string, err error)
+	OnClientMsg(ctx context.Context, method string)
+	OnBackendMsg(ctx context.Context, method string)
+	OnProxyToBackendFinish(ctx context.Context, method string, err error)
+	OnProxyToClientFinish(ctx context.Context, method string, err error)
+	OnDirect(ctx context.Context, method string)
+	OnNewClientStream(ctx context.Context, method string)
+}
+
+type Option func(*handler)
+
+func WithHook(hook Hook) Option {
+	return func(h *handler) {
+		if hook != nil {
+			h.hook = hook
+		}
+	}
+}
+
+func NewHandler(director Director, options ...Option) grpc.StreamHandler {
 	handler := &handler{
 		director: director,
+		hook:     &noopHook{},
+	}
+	for _, option := range options {
+		option(handler)
 	}
 	return handler.ServeStream
 }
@@ -31,23 +96,32 @@ var (
 	}
 )
 
-func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) error {
+func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) (err error) {
 	method, ok := grpc.MethodFromServerStream(srcStream)
+
+	hookCtx := h.hook.OnStart(srcStream.Context(), method)
+	defer h.hook.OnFinish(hookCtx, method, err)
+
 	if !ok {
 		return status.Errorf(codes.InvalidArgument, "method not foun")
 	}
 	destCtx, destConn, err := h.director(srcStream.Context(), method)
 	if err != nil {
-		return err
+		return status.Errorf(codes.Internal, "failed to director,err:%v", err)
 	}
+	h.hook.OnDirect(hookCtx, method)
+
 	defer destConn.Close()
 
 	destCtx, dstCancel := context.WithCancel(destCtx)
 	defer dstCancel()
+
 	destStream, err := grpc.NewClientStream(destCtx, proxyDesc, destConn, method)
 	if err != nil {
-		return err
+		return status.Errorf(codes.Internal, "failed to NewClientStream,err:%v", err)
 	}
+	h.hook.OnNewClientStream(hookCtx, method)
+
 	defer destStream.CloseSend()
 
 	wg := &sync.WaitGroup{}
@@ -61,6 +135,7 @@ func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) erro
 				destStream.CloseSend()
 				return
 			}
+			h.hook.OnProxyToBackendFinish(hookCtx, method, err)
 			dstCancel()
 			errCh <- err
 		}()
@@ -70,6 +145,8 @@ func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) erro
 			if err = srcStream.RecvMsg(msg); err != nil {
 				return
 			}
+			h.hook.OnClientMsg(hookCtx, method)
+
 			if err := destStream.SendMsg(msg); err != nil {
 				return
 			}
@@ -84,6 +161,7 @@ func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) erro
 			if err == io.EOF {
 				return
 			}
+			h.hook.OnProxyToClientFinish(hookCtx, method, err)
 			errCh <- err
 		}()
 		count := 0
@@ -93,6 +171,8 @@ func (h *handler) ServeStream(srv interface{}, srcStream grpc.ServerStream) erro
 			if err := destStream.RecvMsg(msg); err != nil {
 				return
 			}
+			h.hook.OnBackendMsg(hookCtx, method)
+
 			if count == 0 {
 				md, err := destStream.Header()
 				if err != nil {
